cdcc5bba25ad5678b05d2a9063f9f47282ea69a1
wexmachina <106637824+wexmachina@users.noreply.github.com>
2022-09-04 19:05:36 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,11 +1,9 @@
-let getComputerChoice = Math.floor(Math.random() * 4)
+let getComputerChoice=Math.floor(Math.random()*3+1)
 
-for (let getComputerChoice = 1;2;3) {
   if (getComputerChoice == 1) {
     console.log("Rock");
 } else if (getComputerChoice == 2) {
     console.log("Paper");
-} else if (getComputerChoice == 1) {
+} else if (getComputerChoice == 3) {
     console.log("Scissors");
-}
 }
