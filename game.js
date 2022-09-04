8b3a7c0bef665d9d1cc342f129a2293225662b49
wexmachina <106637824+wexmachina@users.noreply.github.com>
2022-09-04 18:32:34 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,11 @@
+let getComputerChoice = Math.floor(Math.random() * 4)
+
+for (let getComputerChoice = 1;2;3) {
+  if (getComputerChoice == 1) {
+    console.log("Rock");
+} else if (getComputerChoice == 2) {
+    console.log("Paper");
+} else if (getComputerChoice == 1) {
+    console.log("Scissors");
+}
+}
